eac6d9b3a9ad5505b03d08b9b94c6d7a231cc91f
WD-94 <d.wegelin@web.de>
2022-12-26 09:17:10 +0100

    FEAT Gameround

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ function getComputerChoice() {
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+  let playerSelect = playerSelection.toLowerCase();
+  playerSelect = playerSelect.charAt(0).toUpperCase() + playerSelect.slice(1);
+  let loose = `You Lose! ${computerSelection} beats ${playerSelect}`;
+  let win = `You win! ${playerSelect} beats ${computerSelection}`;
 
-
+  if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+    return 'Even!';
+  }
+  else if (
+  playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors'
+  || playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock'
+  || playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase()) {
+    return win;
+    }
+  else {
+    return loose;
+  }
+}
