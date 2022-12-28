73a0dd501d2f695db23abb1ab051165a3252801d
WD-94 <d.wegelin@web.de>
2022-12-28 13:20:08 +0100

    FEAT Game of 5 Rounds

--- index.js
+++ index.js
@@ -10,15 +10,43 @@ function playRound(playerSelection, computerSelection) {
   let win = `You win! ${playerSelect} beats ${computerSelection}`;
 
   if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-    return 'Even!';
+    alert('Even!');
+    console.log('Nobody won this round!')
+    return roundPoints = '!add';
   }
   else if (
   playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors'
   || playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock'
   || playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase()) {
-    return win;
+    alert(win);
+    console.log('Player won this round!');
+    return roundPoints = 'add';
     }
   else {
-    return loose;
+    alert(loose);
+    console.log('Computer won this round!');
+    return roundPoints = '!add';
   }
 }
+
+function game() {
+  let points = 0;
+  for (let i = 0; i < 5; i++) {
+    playRound(prompt('Choose either: Rock, Paper, Scissors'), getComputerChoice()); 
+    if (roundPoints === 'add') {
+      points = ++points;
+    } 
+    
+    console.log(`You have ${points}/5 points!`);
+  }
+  if (points > 2) {
+    console.log(`You won the game, with ${points}/5 points!`);
+    alert(`You won the game!`);
+  }
+  else {
+    console.log(`You lost the game, with ${points}/5 points!`);
+    alert('You lost the game!');
+  }
+}
+
+game()
